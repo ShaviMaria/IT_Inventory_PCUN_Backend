8042d2e3c075f3bc9a5eb409477c49bedaa97a69
Javier Alfonso Maria Salas <javier.maria.salas@gmail.com>
2023-02-06 18:49:58 -0500

    Implement cors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,13 @@ app.set('port', 4000);
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // Routes
 //app.use('/api/system_Users:user/:password',  systemUserRoutes);
--- src/controllers/system_User.controller.js
+++ src/controllers/system_User.controller.js
@@ -3,18 +3,34 @@ import { getConnection } from '../database/database';
 const getSystem_Users = async(req, res) => {
     try {
         //const { Windows_User, Password } = req.query;
+        //req.accepts(['json', 'text'])
+        // => "json"
+        //req.accepts('application/json')
+        // => "application/json"
+
+        // Accept: text/*, application/json
+        //req.accepts('image/png')
+        //req.accepts('png')
+        // => false
+
+        // Accept: text/*;q=.5, application/json
+        //req.accepts(['html', 'json'])
+        // => "json"
+        //res.set({
+            //"Content-Type": "application/json",
+            //"Access-Control-Allow-Origin": "*",
+        //});
         const Windows_User = req.params.user;
         const Password = req.params.password;
-        console.log('Windows_User: ', Windows_User);
-        console.log('Password: ', Password);
+        //console.log('Windows_User: ', Windows_User);
+        //console.log('Password: ', Password);
         //const { Windows_User, Password } = req.body;
         const params = { Windows_User, Password };
-        console.log(params);
+        //console.log(params);
         const connection = await getConnection();
         //const result = await connection.query("SELECT * FROM System_Users WHERE Windows_User = ? AND Password = ?", params); 
         const query = `SELECT * FROM System_Users WHERE Windows_User = '${params.Windows_User}' AND Password = '${params.Password}'`;
         const result = await connection.query(query);
-        console.log(result);
         res.json(result);
     } catch (error) {
         res.status(500);
